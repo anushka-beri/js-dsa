65ca66e0b0bd9ee04970b16c7a0d7ad9d6a534f1
Anushka Beri <berianushka@gmail.com>
2019-07-23 18:46:37 +0530

    Checked performance

--- Big-O/findnemo.js
+++ Big-O/findnemo.js
@@ -1,12 +1,15 @@
 const nemo = ['nemo' , 'dory'];
 
 function findNemo(array){
+    let t0 = performance.now();
     for(i=0; i< array.length ; i++ ){
         if(array[i] === 'nemo'){
             console.log('Found NEMO!');
         }
     }
-    console.log(array.length);
+    let t1 = performance.now();
+    console.log('Call to find both Nemo and Dory took' + (t1-t0) + 'milliseconds');
+    // console.log('length of array = ' + array.length);
 }
 
 findNemo(nemo);
