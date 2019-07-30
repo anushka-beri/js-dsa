2f178d3db4ed840d46fb98a1db6312ee35f64c5d
Anushka Beri <berianushka@gmail.com>
2019-07-30 06:24:14 +0530

    Learnt Constant Time

--- Big-O/findnemo.js
+++ Big-O/findnemo.js
@@ -1,19 +1,28 @@
-const fish = ['dory', 'bruce', 'marlin', 'nemo'];
-const nemo = ['nemo'];
-const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+// const fish = ['dory', 'bruce', 'marlin', 'nemo'];
+// const nemo = ['nemo'];
+// const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
 
-const large = new Array(10).fill('nemo');
+// const large = new Array(10).fill('nemo');
 
-function findNemo2(fish){
-    let t0 = performance.now();
-    for(i=0; i< fish.length ; i++ ){
-        if(array[i] === 'nemo'){
-            console.log('Found NEMO!');
-        }
-    }
-    let t1 = performance.now();
-    console.log('Call to find both Nemo and Dory took' + (t1-t0) + 'milliseconds');
-    // console.log('length of array = ' + array.length);
+// function findNemo2(fish){
+//     let t0 = performance.now();
+//     for(i=0; i< fish.length ; i++ ){
+//         if(array[i] === 'nemo'){
+//             console.log('Found NEMO!');
+//         }
+//     }
+//     let t1 = performance.now();
+//     console.log('Call to find both Nemo and Dory took' + (t1-t0) + 'milliseconds');
+//     // console.log('length of array = ' + array.length);
+// }
+
+// findNemo2(everyone);
+
+const boxes= [0, 1, 2, 3, 4, 5];
+
+function logFirstTwoBoxes(boxes){
+    console.log(boxes[0]); // O(1)
+    console.log(boxes[1]);  // O(1)
 }
 
-findNemo2(everyone);
+logFirstTwoBoxes(boxes); // O(2) ~ O(1)- Constant Time
