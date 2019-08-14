c22d4740d36ea48d3cebe4f23270dadd96438f6f
Anushka Beri <berianushka@gmail.com>
2019-08-15 00:43:32 +0530

    Space complexity learnt.

---
+++ Big-O/spaceComplexity.js
@@ -0,0 +1,17 @@
+function boo(n){
+    for (let i=0; i < n.length ; i++){
+        console.log('ScaryBoo!');
+    }
+}
+
+boo([1,2,3,4,5]); //O(1)
+
+function arrayOfHiNTimes(n){
+    let hiArray = [];
+    for (let i=0; i < n ; i++){
+        hiArray[i] = 'hi';
+    }
+    return hiArray;
+}
+
+arrayOfHiNTimes(6); // O(n)
